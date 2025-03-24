cf002211f4c2c327f38cdf953b5df0391598803e
VladimirBelyaev <vogisa@mail.ru>
2025-03-24 16:32:19 +0300

    added some slots to customization

--- src/components/base/Form.vue
+++ src/components/base/Form.vue
@@ -7,34 +7,40 @@
 				:label="item.label"
 				:show-label="item.type !== 'checkbox'"
 			>
-				<NInput v-if="item.type === 'input'" v-model:value="model[item.key]" v-bind="item.props" />
-				<NSelect
-					v-else-if="item.type === 'select'"
-					v-model:value="model[item.key]"
-					:options="item.options"
-					v-bind="item.props"
-				/>
-				<NCheckbox
-					v-else-if="item.type === 'checkbox'"
-					v-model:checked="model[item.key]"
-					:label="item.label"
-					v-bind="item.props"
-				/>
-				<NInput
-					v-else-if="item.type === 'textarea'"
-					v-model:value="model[item.key]"
-					type="textarea"
-					:autosize="{
-						minRows: 4,
-						maxRows: 8,
-					}"
-					v-bind="item.props"
-				/>
+				<slot :name="`item-${item.key}`">
+					<NInput v-if="item.type === 'input'" v-model:value="model[item.key]" v-bind="item.props" />
+					<NSelect
+						v-else-if="item.type === 'select'"
+						v-model:value="model[item.key]"
+						:options="item.options"
+						v-bind="item.props"
+					/>
+					<NCheckbox
+						v-else-if="item.type === 'checkbox'"
+						v-model:checked="model[item.key]"
+						:label="item.label"
+						v-bind="item.props"
+					/>
+					<NInput
+						v-else-if="item.type === 'textarea'"
+						v-model:value="model[item.key]"
+						type="textarea"
+						:autosize="{
+							minRows: 4,
+							maxRows: 8,
+						}"
+						v-bind="item.props"
+					/>
+				</slot>
 			</NFormItem>
 		</NForm>
 		<div class="flex gap-2 justify-end">
-			<NButton round secondary @click="onClickClear">Clear</NButton>
-			<NButton round type="success" @click="onClickSave">Save</NButton>
+			<slot name="buttons">
+				<slot name="before-buttons"></slot>
+				<NButton round secondary @click="onClickClear">Clear</NButton>
+				<NButton round type="success" @click="onClickSave">Save</NButton>
+				<slot name="after-buttons"></slot>
+			</slot>
 		</div>
 	</div>
 </template>
