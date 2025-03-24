8b97de8a241745caeaf3a534963dba55a7d71791
VladimirBelyaev <vogisa@mail.ru>
2025-03-24 15:22:44 +0300

    user form moved to individual file | created base Page component | added /user route to user form | init links to forms

---
+++ src/components/Forms/UserInfoForm.vue
@@ -0,0 +1,43 @@
+<template>
+	<Form v-model:model="data" class="mt-8" :items="formItems" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import Form from '@/components/base/Form.vue'
+
+import type { FormItem } from '@/types/form'
+
+const data = ref<Record<string, any>>({ name: null, age: null, married: false, info: null })
+
+const formItems: FormItem[] = [
+	{
+		type: 'input',
+		label: 'Name',
+		modelKey: 'name',
+		props: {
+			clearable: true,
+		},
+	},
+	{
+		type: 'select',
+		label: 'Age',
+		modelKey: 'age',
+		options: [...Array(100).keys()].map(i => ({ label: i.toString(), value: i })),
+		props: {
+			clearable: true,
+		},
+	},
+	{
+		type: 'checkbox',
+		label: 'Married',
+		modelKey: 'married',
+	},
+	{
+		type: 'textarea',
+		label: 'Some info',
+		modelKey: 'info',
+	},
+]
+</script>
