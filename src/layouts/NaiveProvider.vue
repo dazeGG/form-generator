90ab8db6301633350234f9670119446eac3be9b4
VladimirBelyaev <vogisa@mail.ru>
2025-03-24 13:08:12 +0300

    removed naive rem convert

--- src/layouts/NaiveProvider.vue
+++ src/layouts/NaiveProvider.vue
@@ -1,5 +1,5 @@
 <template>
-	<NConfigProvider :theme="darkTheme" :theme-overrides="naiveRemConvert">
+	<NConfigProvider :theme="darkTheme">
 		<NNotificationProvider>
 			<NGlobalStyle />
 			<slot></slot>
@@ -9,7 +9,6 @@
 
 <script setup lang="ts">
 import { darkTheme } from 'naive-ui'
-import naiveRemConvert from '@/lib/plugins/naiveRemConvert.ts'
 
 import { NConfigProvider, NNotificationProvider, NGlobalStyle } from 'naive-ui'
 </script>
