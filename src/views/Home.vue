fab4cc53b4a7d6a4c291494bcb6c637e2191c623
VladimirBelyaev <vogisa@mail.ru>
2025-03-24 03:17:18 +0300

    styles and tailwindcss refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex flex-col gap-4">
 		<h1>Home page</h1>
-		<NButton type="primary" round>Test button</NButton>
+		<NButton type="primary" round class="mt-10">Test button</NButton>
 	</div>
 </template>
 
